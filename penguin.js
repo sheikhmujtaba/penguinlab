cf29a59550b38b37c7243ba8204c6cab53e35182
unknown <muji88851@gmail.com>
2019-10-23 23:54:37 -0400

    work

--- penguin.js
+++ penguin.js
@@ -1,42 +1,106 @@
-var PengPromise = d3.json("classData.json")
-PengPromise.then(function(penguin)
-{
-console.log("penguin", penguin)
-    Getimage(penguin)
-    console.log(alldata)
-    
-},
-function(err)
-{
-console.log("fail", err)
-})
-
-var Getimage = function(penguin)
-{
-d3.select("#table")
-    .append("tr")
-    .selectAll("tr")
-    .data(penguin)
-    .enter()
-    .append("tr")
-    .append("td")
-    .append("img")
-    .attr("src", function(d)
-         {
-    return "/penguins/"+d.picture;
-})
-    
-}
-var anygrade = function(subject)
+var penguinPromise = d3.json("penguins/classData.json")
+    penguinPromise.then(
+        function(penguins)
+        {
+            Maketable(penguins)
+            GetImage(penguins)
+            Quiz(penguins)
+            HW(penguins)
+            Tests(penguins)
+            finalgrade(penguins)
+        },
+        
+        function(err)
+        {
+        console.log("fail", err)
+        })     
+var Maketable = function(penguins)
     {
-        return subject.grade;
+        d3.select("tbody")
+            .selectAll("tr")
+            .data(penguins)
+            .enter()
+            .append("tr")
     }
-var Getdata = function(penguin)
+
+var GetImage = function(penguins)
+    {
+        d3.selectAll("tbody>tr")
+        .append("img")
+        .attr("src", function(penguins)
+            {
+              return "penguins/" + penguins.picture;
+            })
+        
+    {
+        d3.selectAll("tbody>tr")
+        .append("td")
+        .text(function(grades)
+        {
+            return Quiz(grades)
+            
+         })
+    }
+    {
+        d3.selectAll("tbody>tr")
+        .append("td")
+        .text(function(grades)
+        {
+            return HW(grades)
+        })
+    }
+    {
+        d3.selectAll("tbody>tr")
+        .append("td")
+        .text(function(grades)
+        {
+            return Tests(grades)
+        })
+    }
+        {
+        d3.selectAll("tbody>tr")
+        .append("td")
+        .text(function(penguins)
+        {
+           var finalgrade = d3.mean(penguins.quizes, getGrade)*.2 + d3.mean(penguins.homework, getGrade)*.15 + d3.mean(penguins.test, getGrade)*.3 + penguins.final[0].grade*.35
+         
+         return finalgrade
+        })
+             .style("background-color",function(penguins)
+    {
+         var finalgrade = d3.mean(penguins.quizes, getGrade)*.2 + d3.mean(penguins.homework, getGrade)*.15 + d3.mean(penguins.test, getGrade)*.3 + penguins.final[0].grade*.35
+         
+         
+    if(finalgrade < 70)
+        {
+             return "red";  
+        }
+    
+    })
+            
+    }
+    }
+
+var getGrade = function(quiz)
 {
-   var data = {}
-   data.quizmean = d3.mean(penguin.quizes, anygrade)
-    data.hwmean = d3.mean(penguin.homework, anygrade)
-    data.testmean = d3.mean(penguin.test, anygrade)
-    return data
+    return quiz.grade;
 }
-var alldata = d3.map(Getdata)
+
+var Quiz = function(peng)
+{
+
+    return d3.mean(peng.quizes, getGrade)
+} 
+
+var HW = function(peng)
+{
+    return d3.mean(peng.homework, getGrade)
+}
+
+var Tests = function(peng)
+{
+    return d3.mean(peng.test, getGrade)
+}
+
+
+    
